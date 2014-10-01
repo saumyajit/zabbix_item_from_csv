99ef7befe8e8660d87cae7c5f3da251dee6ad669
Zubair AHMED <zubayr.a@gmail.com>
2014-10-01 21:30:14 +0530

    Adding Test file.

---
+++ zabbix_item_test.py
@@ -0,0 +1,16 @@
+import zabbix_items_from_csv
+from xml.etree import ElementTree
+
+complete_list_dict_device_1 =  zabbix_items_from_csv.reader_csv_file('oid_list_with_range_processed.csv', 10)
+xml_tree_device_1 = zabbix_items_from_csv.generate_items_xml_file_complete(complete_list_dict_device_1, 'BLR-DEVICE_1', 'BLR-DEVICE_1', '10.12.51.11', 'DEVICE_1')
+zabbix_items_from_csv.xml_pretty_me('BLR-DEVICE_1.xml', ElementTree.tostring(xml_tree_device_1))
+
+xml_tree_device_1 = zabbix_items_from_csv.generate_items_xml_file_complete(complete_list_dict_device_1, 'CHN-DEVICE_1', 'CHN-DEVICE_1', '10.12.51.11', 'DEVICE_1')
+zabbix_items_from_csv.xml_pretty_me('CHN-DEVICE_1.xml', ElementTree.tostring(xml_tree_device_1))
+
+complete_list_dict_device_2 =  zabbix_items_from_csv.reader_csv_file('oid_list_with_range_processed.csv')
+xml_tree_device_2 = zabbix_items_from_csv.generate_items_xml_file_complete(complete_list_dict_device_2, 'BLR-DEVICE_2', 'BLR-DEVICE_2', '12.12.54.66', 'DEVICE_2')
+zabbix_items_from_csv.xml_pretty_me('BLR-DEVICE_2.xml', ElementTree.tostring(xml_tree_device_2))
+
+xml_tree_device_2 = zabbix_items_from_csv.generate_items_xml_file_complete(complete_list_dict_device_2, 'CHN-DEVICE_2', 'CHN-DEVICE_2', '12.12.52.74', 'DEVICE_2')
+zabbix_items_from_csv.xml_pretty_me('CHN-DEVICE_2.xml', ElementTree.tostring(xml_tree_device_2))
